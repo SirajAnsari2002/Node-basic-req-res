28e429e3c57cc71ff7040d3dbacbb8d70d40c314
Ansari Siraj <sirajrolln@gmail.com>
2024-11-26 16:48:33 +0530

    Accept user input using simple form

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const SERVER = HTTP.createServer((req, res)=>{
     if(req.url === '/'){
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
-    res.write('<head><title>Server</title></head>');
-    res.write('<body>This is the home page of our website....</body>');
-    res.write('</html>')
+    res.write('<head><title>Welcome to home page</title></head>');
+    res.write('<body>');
+    res.write('<h1>Please Enter your details...</h1>');
+    res.write('<form action = "/content-url" method = "POST">');
+    res.write('<input type = "text" id = "name" name = "username" placeholder = "enter your name..">');
+    res.write('<label for = "male">male</label>');
+    res.write('<input type = "radio" id = "male" name = "gender" value = "male">');
+    res.write('<label for = "female">female</label>');
+    res.write('<input type = "radio" id = "female" name = "gender" value = "female">');
+    res.write('<input type = "submit" value = "submit">');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
     return res.end();
     }else if(req.url === '/books'){
     res.setHeader('Content-Type', 'text/html');
@@ -28,6 +38,8 @@ const SERVER = HTTP.createServer((req, res)=>{
     // console.log(req);
 
 });
+
+
 const PORT = 3000;
 SERVER.listen(PORT, ()=>{
     console.log(`server running on address http://localhost:${PORT}`);
