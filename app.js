214d4f7ef0ff686af7987d3d541ea64a12d70d62
Ansari Siraj <sirajrolln@gmail.com>
2024-11-26 16:17:02 +0530

    Create the routing using the differebt URL

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const HTTP = require('http');
 
 const SERVER = HTTP.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
+    if(req.url === '/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>Server</title></head>');
+    res.write('<body>This is the home page of our website....</body>');
+    res.write('</html>')
+    return res.end();
+    }else if(req.url === '/books'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>Server</title></head>');
+    res.write('<body>select the books you want to read...</body>');
+    res.write('</html>')
+    return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>Server</title></head>');
